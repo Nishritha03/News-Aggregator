e6f552d9d1c11489ebb7ad1b8bc4334001a69b10
Nishritha03 <161455129+Nishritha03@users.noreply.github.com>
2024-09-04 15:54:23 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Api, Resource
+import csv
+from datetime import datetime
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+    
+    from .routes import ArticleList, ArticleDetail, ArticleSearch
+
+    # Defining API routes
+    api.add_resource(ArticleList, '/articles')
+    api.add_resource(ArticleDetail, '/articlesid')
+    api.add_resource(ArticleSearch, '/search')
+
+    @app.route('/')
+    def home():
+        # Renders the home.html template when accessing the root URL
+        return render_template('home.html')
+
+    return app
 
